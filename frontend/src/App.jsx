365f3f42a430f11ba2114deffc556c0dc85e760d
OlivierBesnard <olivierbesnardvii@gmail.com>
2025-10-20 15:09:54 +0200

    can display all content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<ResultsList resultsArray={sampleData.Shop} />
+			<ResultsList resultsArray={sampleData.City} type={"city"}/>
 		</>
 	);
 }
--- frontend/src/ResultsList.jsx
+++ frontend/src/ResultsList.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 
-const ResultsList = ({ resultsArray }) => {
+const ResultsList = ({ resultsArray, type }) => {
+	
 	return (
 		<div className="results-list">
 			<h2>Results</h2>
 			<div className="results-container">
-				{resultsArray.map((item, index) => (
-					<div key={index} className="result-item">
-						<h3>{item.name}</h3>
-						<p>{item.address}</p>
-					</div>
-				))}
+				{resultsArray.map((item, index) =>
+					type === "shop" ? (
+						<div key={index} className="result-item">
+							<h3>{item.name}</h3>
+							<p>{item.address}</p>
+							<p>{item.url}</p>
+						</div>
+					) : 
+					type === "city" ? (
+						<div key={index} className="result-item">
+							<h3>{item.name}</h3>
+							<p>{item.postal_code}</p>
+						</div>
+					) :
+					type === "product" ? (
+						<div key={index} className="result-item">
+							<h3>{item.name}</h3>
+						</div>
+					) : null
+				)}
 			</div>
 		</div>
 	);
