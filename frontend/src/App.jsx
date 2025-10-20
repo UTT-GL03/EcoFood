69eef860adaa06fdc80a2d65af15fdeded824c94
OlivierBesnard <olivierbesnardvii@gmail.com>
2025-10-20 17:20:29 +0200

    add SearchCart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,19 @@ import "./App.css";
 import sampleData from "../sample_data.json";
 import ResultsList from "./ResultsList";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Button } from 'react';
+import { useState } from "react";
+import SearchCart from "./SearchCart";
+
+
 
 function App() {
+  const [adults, setAdults] = useState(0);
+  const [children, setChildren] = useState(0);
+  const [city, setCity] = useState("");
+  const onclick = () => {
+    console.log(city);
+    console.log(`/results/${city}/paniermoyen?nombre_adultes=${adults}&nombre_enfants=${children}`);
+  }
 	return (
 		<>
       <BrowserRouter >
@@ -16,10 +26,9 @@ function App() {
         </div>
         <Routes>
           <Route path="/" element={
-            <div className="navigation">
-              <Link to="/results/city">
-                <button>Go to Results</button>
-              </Link>
+            <div>
+            <SearchCart>
+              </SearchCart>
             </div>
           }></Route>
           <Route path="/results/city" element={<ResultsList resultsArray={sampleData.City} type={"city"} />} />
