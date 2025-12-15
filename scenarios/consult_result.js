a8a5f7aa38c0e8555326a4c1ae1d173b0cc67e8f
Veitzen <hoang_viet.le@utt.fr>
2025-12-15 15:17:09 +0100

    fix : fix url of test

--- scenarios/consult_result.js
+++ scenarios/consult_result.js
@@ -1,11 +1,11 @@
 const visit = async (page) => {
-  await page.goto('results/1/1', {
-      waitUntil: 'networkidle',
-  });
-  await page.waitForTimeout(10000);
-  await page.scrollToEnd();
-  await page.waitForNetworkIdle();
-  await page.waitForTimeout(7000);
+	await page.goto("results/city_1/product_1", {
+		waitUntil: "networkidle",
+	});
+	await page.waitForTimeout(10000);
+	await page.scrollToEnd();
+	await page.waitForNetworkIdle();
+	await page.waitForTimeout(7000);
 };
 
 module.exports = visit;
